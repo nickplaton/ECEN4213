4404e518db6da1c0520a899a709ad0724b871c36
nickplaton <nplaton@okstate.edu>
2025-10-07 18:26:23 -0500

    finished lab 3 bonus

--- Lab 3/code/Lab3BonusB.cpp
+++ Lab 3/code/Lab3BonusB.cpp
@@ -21,48 +21,6 @@ int createSocket();
 
 int sock = 0;
 
-int main(int argc, char const *argv[]){
-	
-	//Open the file stream for the joystick
-	Joystick joystick("/dev/input/js0");
-	if(!joystick.isFound()){
-		cout << "Error opening joystick" << endl;
-		exit(1);
-	}
-
-
-	//Create the connection to the server
-	createSocket();
-
-	while(true){
-
-		/*Sample the events from the joystick*/
-
-		/*Convert the event to a useable data type so it can be sent*/
-		/*Scale the value to a range the Kobuki can handle
-		No more than 800mm/s*/
-		/*Scale the value of radius to a smaller max value
-		Max radius should be about 1 meter*/
-
-		/*Print the data stream to the terminal*/
-
-		/*Send the data to the server*/
-
-		if(/**/) {
-		/*Closes out of all connections cleanly*/
-
-		//When you need to close out of the connection, please
-		//close TTP/IP data streams.
-		//Not doing so will result in the need to restart
-		//the raspberry pi and Kobuki
-			close(sock);
-			exit(0);
-
-		/*Set a delay*/
-	}
-	return 0;
-}
-
 //Creates the connection between the client and
 //the server with the controller being the client
 int createSocket(){
@@ -80,7 +38,7 @@ int createSocket(){
 	serv_addr.sin_port   = htons(PORT);
 
 	/*Use the IP address of the server you are connecting to*/
-	if(inet_pton(AF_INET, "XX.XX.XX.XX" , &serv_addr.sin_addr) <= 0){
+	if(inet_pton(AF_INET, "10.227.31.128" , &serv_addr.sin_addr) <= 0){
 		printf("\nInvalid address/ Address not supported \n");
 		return -1;
 	}
@@ -88,6 +46,86 @@ int createSocket(){
 	if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
 		printf("\nConnection Failed \n");
 		return -1;
+	}
+	return 0;
+}
+
+int main(int argc, char const *argv[]){
+	
+	//Open the file stream for the joystick
+	Joystick joystick("/dev/input/js0");
+	JoystickEvent event;
+	if(!joystick.isFound()){
+		cout << "Error opening joystick" << endl;
+		exit(1);
+	}
+
+	int sp = 250;
+	int r = 500;
+	int b = 230;
+	int w = 1;
+
+	//Create the connection to the server
+	createSocket();
+	int speed, radius = 0;
+
+	while(true){
+		// axis 1 is front back, negative front positive back
+		// axis 3 is right stick front back 
+		/*Sample the events from the joystick*/
+		if (joystick.sample(&event))
+		{
+			if (event.isButton())
+			{
+				printf("isButton: %u | Value: %d\n", event.number, event.value);
+				/*Interpret the joystick input and use that input to move the Kobuki*/
+				if (event.number == 7 && event.value == 1){
+					speed = 0;
+					radius = 0;
+					cout<<"stopping";
+				}
+				if (event.number == 8 && event.value == 1){
+					speed = -1;
+					radius = -1;
+					cout<<"closing connection";
+					return(0);
+				}
+			}
+			if (event.isAxis())
+			{
+				printf("isAxis: %u | Value: %d\n", event.number, event.value);
+				/*Interpret the joystick input and use that input to move the Kobuki*/
+				if (event.number == 1){
+					cout<<"forward/backward";
+					speed = sp*(float(event.value)/-32767);
+				}
+				else if (event.number == 3){
+					cout<<"turning";
+					radius = 500-abs(499*(float(event.value)/-32767));
+					if (radius == 500) radius = 0;
+				}
+			}
+		}
+		int data[2] = {speed, radius};
+
+		/*Print the data stream to the terminal*/
+		cout<<"Data to be sent:\n\tSpeed: "<<data[0]<<"\n\tRadius: "<<data[1]<<endl;
+		/*Send the data to the server*/
+		send(sock, data, sizeof(data), 0);
+
+		if(speed == -1) {
+		/*Closes out of all connections cleanly*/
+
+		//When you need to close out of the connection, please
+		//close TTP/IP data streams.
+		//Not doing so will result in the need to restart
+		//the raspberry pi and Kobuki
+			close(sock);
+			exit(0);
+
+		}
+		/*Set a delay*/
+		usleep(20e3);
 	}
 	return 0;
 }
--- Lab 3/code/Lab3EX3B.cpp
+++ Lab 3/code/Lab3EX3B.cpp
@@ -19,45 +19,6 @@ int createSocket();
 
 int sock = 0;
 
-int main(int argc, char const *argv[]){
-	
-	//Open the file stream for the joystick
-	Joystick joystick("/dev/input/js0");
-	JoystickEvent event;
-	if(!joystick.isFound()){
-		cout << "Error opening joystick" << endl;
-		exit(1);
-	}
-
-
-	//Create the connection to the server
-	createSocket();
-
-	while(true){
-
-		/*Sample the events from the joystick*/
-
-		/*Convert the event to a useable data type so it can be sent*/
-
-		/*Print the data stream to the terminal*/
-
-		/*Send the data to the server*/
-
-		if(/**/) {
-		/*Closes out of all connections cleanly*/
-
-		//When you need to close out of the connection, please
-		//close TTP/IP data streams.
-		//Not doing so will result in the need to restart
-		//the raspberry pi and Kobuki
-			close(sock);
-			exit(0);
-
-		/*Set a delay*/
-	}
-	return 0;
-}
-
 //Creates the connection between the client and
 //the server with the controller being the client
 int createSocket(){
@@ -75,7 +36,7 @@ int createSocket(){
 	serv_addr.sin_port   = htons(PORT);
 
 	/*Use the IP address of the server you are connecting to*/
-	if(inet_pton(AF_INET, "XX.XX.XX.XX" , &serv_addr.sin_addr) <= 0){
+	if(inet_pton(AF_INET, "10.227.31.128" , &serv_addr.sin_addr) <= 0){
 		printf("\nInvalid address/ Address not supported \n");
 		return -1;
 	}
@@ -83,6 +44,99 @@ int createSocket(){
 	if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
 		printf("\nConnection Failed \n");
 		return -1;
+	}
+	return 0;
+}
+
+int main(int argc, char const *argv[]){
+	
+	//Open the file stream for the joystick
+	Joystick joystick("/dev/input/js0");
+	JoystickEvent event;
+	if(!joystick.isFound()){
+		cout << "Error opening joystick" << endl;
+		exit(1);
+	}
+
+	int sp = 250;
+	int r = 500;
+	int b = 230;
+	int w = 1;
+
+	//Create the connection to the server
+	createSocket();
+	int speed, radius = 0;
+
+	while(true){
+		/*Sample the events from the joystick*/
+		if (joystick.sample(&event))
+		{
+			if (event.isButton())
+			{
+				printf("isButton: %u | Value: %d\n", event.number, event.value);
+				/*Interpret the joystick input and use that input to move the Kobuki*/
+				if (event.number == 7 && event.value == 1){
+					speed = 0;
+					radius = 0;
+					cout<<"stopping";
+				}
+				if (event.number == 8 && event.value == 1){
+					speed = -1;
+					radius = -1;
+					cout<<"closing connection";
+					return(0);
+				}
+			}
+			if (event.isAxis())
+			{
+				printf("isAxis: %u | Value: %d\n", event.number, event.value);
+				/*Interpret the joystick input and use that input to move the Kobuki*/
+				if (event.number == 5 && event.value < 0){
+					cout<<"forward";
+					speed = sp;
+					radius = 0;
+				}
+				else if (event.number == 5 && event.value > 0){
+					cout<<"backward";
+					speed = -sp;
+					radius = 0;
+				}
+				else if (event.number == 4 && event.value < 0){
+					cout<<"right";
+					speed = w*b/2;
+					radius = 1;
+				}
+				else if (event.number == 4 && event.value > 0){
+					cout<<"left";
+					speed = -w*b/2;
+					radius = 1;
+				}
+				else if (event.value == 0){
+					speed = 0;
+					radius = 0;
+				}
+			}
+		}
+		int data[2] = {speed, radius};
+
+		/*Print the data stream to the terminal*/
+		cout<<"Data to be sent:\n\tSpeed: "<<data[0]<<"\n\tRadius: "<<data[1]<<endl;
+		/*Send the data to the server*/
+		send(sock, data, sizeof(data), 0);
+
+		if(speed == -1) {
+		/*Closes out of all connections cleanly*/
+
+		//When you need to close out of the connection, please
+		//close TTP/IP data streams.
+		//Not doing so will result in the need to restart
+		//the raspberry pi and Kobuki
+			close(sock);
+			exit(0);
+
+		}
+		/*Set a delay*/
+		usleep(20e3);
 	}
 	return 0;
 }
